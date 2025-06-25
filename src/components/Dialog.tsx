8d8be6a1a294aa77448d2ec3e4b8648f1d638724
LeeYunHwan <cyzhqly@gmail.com>
2025-06-26 00:09:59 +0900

    feat: 기본 Dialog 구현

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,10 @@
+export default function Dialog({ onClose }: { onClose: () => void }) {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center bg-black/30 z-50">
+      <div className="relative flex flex-col justify-center items-center w-[200px] h-[200px] bg-amber-100 rounded shadow-lg">
+        <button onClick={onClose} className="absolute top-2 right-2 text-red-500 hover:text-red-700">X</button>
+        <p>Dialog</p>
+      </div>
+    </div>
+  );
+}
